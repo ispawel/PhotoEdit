eee1906ba81170c1c06ad7dab0b3e10646c523a9
Pavel Isakov <81164077+ispawel@users.noreply.github.com>
2021-10-27 01:15:51 +0700

    Edit mvc

--- PhotoEdit/ViewController.swift
+++ PhotoEdit/ViewController.swift
@@ -7,17 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var addButton: UIButton!
    
+    @IBOutlet weak var picture: UIImageView!
+    
+    
+    
+    private var imagePicker: ImagePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
        
     }
 
+   
+    
+    
+    @IBAction func edit(_ sender: Any) {
+    }
+    
     @IBAction func addAction(_ sender: Any) {
+        self.imagePicker.present(from: sender as! UIView)
     }
     
 }
 
+extension ViewController: ImagePickerDelegate{
+    
+    func didSelect(image: UIImage?) {
+        self.picture.image = image
+    }
+    
+    
+}
