bd34e1fd9ed8173817222ce703793ee66fb678da
Pavel Isakov <81164077+ispawel@users.noreply.github.com>
2021-10-29 00:00:57 +0700

    add filters

--- PhotoEdit/Editor/EditorViewController.swift
+++ PhotoEdit/Editor/EditorViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class EditorViewController: UIViewController {
 
+    @IBOutlet weak var FilterView: UIView!
+   
     public var picture: UIImage?
     
     @IBOutlet weak var imageView: UIImageView!
@@ -17,10 +19,22 @@ class EditorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.imageView.image = picture
+        FilterView.backgroundColor = UIColor(displayP3Red: 0, green: 0, blue: 0, alpha: 0)
 
     }
     
-
+    @IBAction func action1(_ sender: Any) {
+        FilterView.backgroundColor = UIColor.Filter.orange
+    }
+    @IBAction func action2(_ sender: Any) {
+        FilterView.backgroundColor = UIColor.Filter.blue
+    }
+    @IBAction func action3(_ sender: Any) {
+        FilterView.backgroundColor = UIColor.Filter.purple
+    }
+    @IBAction func action4(_ sender: Any) {
+        FilterView.backgroundColor = UIColor.Filter.yellow
+    }
    
 
 }
---
+++ PhotoEdit/UIColorrr.swift
@@ -0,0 +1,18 @@
+//
+//  UIColorrr.swift
+//  PhotoEdit
+//
+//  Created by Pavel Isakov on 28.10.2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    struct Filter{
+        static let yellow = UIColor(displayP3Red: 0.988, green: 0.883, blue: 0.656, alpha: 0.13)
+        static let purple = UIColor(displayP3Red: 0.721, green: 0.655, blue: 0.989, alpha: 0.13)
+        static let blue = UIColor(displayP3Red: 0.161, green: 0.464, blue: 0.483, alpha: 0.13)
+        static let orange = UIColor(displayP3Red: 0.483, green: 0.258, blue: 0.161, alpha: 0.13)
+    }
+}
